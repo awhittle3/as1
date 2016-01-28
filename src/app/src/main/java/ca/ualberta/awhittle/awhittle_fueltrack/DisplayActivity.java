82c8693c5af46db659458260681e596da19fa14d
Andrew Whittle <awhittle@ualberta.ca>
2016-01-28 10:08:14 -0700

    ANDR: Make Display layout

--- src/app/src/main/java/ca/ualberta/awhittle/awhittle_fueltrack/DisplayActivity.java
+++ src/app/src/main/java/ca/ualberta/awhittle/awhittle_fueltrack/DisplayActivity.java
@@ -2,7 +2,6 @@ package ca.ualberta.awhittle.awhittle_fueltrack;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class DisplayActivity extends AppCompatActivity {
@@ -11,8 +10,6 @@ public class DisplayActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_display);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
     }
 
--- src/app/src/main/java/ca/ualberta/awhittle/awhittle_fueltrack/EditEntryActivity.java
+++ src/app/src/main/java/ca/ualberta/awhittle/awhittle_fueltrack/EditEntryActivity.java
@@ -2,7 +2,6 @@ package ca.ualberta.awhittle.awhittle_fueltrack;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class EditEntryActivity extends AppCompatActivity {
@@ -11,8 +10,6 @@ public class EditEntryActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_entry);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
     }
 
